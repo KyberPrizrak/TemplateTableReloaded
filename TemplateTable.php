790495d9f8cbdb3ff2b9f545fea0288aa8d24ddd
Rusty Burchfield <GICodeWarrior@gmail.com>
2015-03-13 15:48:01 -0700

    Version 1.1 release.

--- TemplateTable.php
+++ TemplateTable.php
@@ -11,7 +11,7 @@
 $wgExtensionCredits['parserhook'][] = array(
   'path' => __FILE__,
   'name' => 'TemplateTableReloaded',
-  'version' => '1.0',
+  'version' => '1.1',
   'url' => 'https://www.mediawiki.org/wiki/Extension:TemplateTableReloaded',
   'author' => array('CS "Kainaw" Wagner', 'Rusty Burchfield'),
   'description' => 'Render a table of parameters passed to a template.',
