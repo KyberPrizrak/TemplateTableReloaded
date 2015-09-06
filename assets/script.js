defb92c0d9311795b5e161c5ae406b1f3b592b05
Rusty Burchfield <GICodeWarrior@gmail.com>
2015-09-06 08:58:13 -0700

    Fix purge links on pages with additional query params.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,10 @@
 $(function() {
+  var query = location.search;
+  query = query.length ? query + '&' : '?'
+  query += 'action=purge';
+
   $('table.ttable th:last-child').append($('<a>').attr({
-    'href': '?action=purge',
+    'href': query,
     'title': 'Clear page cache (refresh all tables on this page)'
   }));
 });
